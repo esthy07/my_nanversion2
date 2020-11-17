7c40be9982b8a4c7f9e82a8afed7a6881b98392a
pentest225 <patrick.dagouaga@uvci.edu.ci>
2020-11-17 14:09:54 +0000

    hide debug barner

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'My NaN ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
