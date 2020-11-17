0b4f697a69326c5b966dcb05c41bd9fef505ad0b
esthy07 <akissi.ngoran@uvci.edu.ci>
2020-11-17 12:02:05 +0000

    homepage

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-
 class HomePage extends StatefulWidget {
 
   static const routeName = 'homePage';
@@ -17,38 +16,15 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     final deviceWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search, color: Colors.purple,),
-              title: Text('Rechercher', style: TextStyle(
-                fontWeight: FontWeight.w300
-              ),)
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.mail, color: Colors.purple,),
-                title: Text('Message', style: TextStyle(
-                    fontWeight: FontWeight.w300
-                ),)
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Colors.purple,),
-                title: Text('Profils', style: TextStyle(
-                    fontWeight: FontWeight.w300
-                ),)
-            )
-          ]
-      ),
       body: Container(
         height: deviceHeight,
         width: deviceHeight,
         decoration: BoxDecoration(
           //color: Colors.pink,
-          image: DecorationImage(
-            image: AssetImage('assets/images/bbb.jpg'),
-            fit: BoxFit.cover
-          )
+            image: DecorationImage(
+                image: AssetImage('assets/images/bbb.jpg'),
+                fit: BoxFit.cover
+            )
         ),
         child: Padding(
           padding: EdgeInsets.only(left: deviceWidth * .05, right: deviceWidth * .03),
@@ -56,60 +32,78 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
-                margin: EdgeInsets.only(top: deviceHeight * .1,),
-                child: Text('Bienvenue sur MyNaN')
-                /*TypewriterAnimatedTextKit(
-                    speed: Duration(seconds: 2),
-                    text: ['Bienvenue sur MyNaN'],
-                    textStyle : TextStyle(
-                        color: Colors.black, fontSize: 35, fontFamily: "Agne"
-                    ),  textAlign: TextAlign.start,
-                    alignment: AlignmentDirectional.topStart )*/
+                  margin: EdgeInsets.only(top: deviceHeight * .1,),
+                  child: Text('Bienvenue sur MyNaN', style: TextStyle(
+                      color: Colors.white, fontSize: 25
+                  ),)
               ),
               Container(
                 margin: EdgeInsets.only(top: deviceHeight * .015),
                 child: Text('Vendredi 20 novembre', style: TextStyle(
-                  color: Colors.white
+                    color: Colors.white
                 ),),
               ),
               Container(
-                padding: EdgeInsets.only(left: deviceWidth * .05),
-                margin: EdgeInsets.only(top: deviceHeight * .08),
-                height: deviceHeight * .2,
-                width: deviceWidth,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                  borderRadius: BorderRadius.circular(5),
-                ),
+                margin: EdgeInsets.only(top: deviceHeight * .15),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Container(
-                      margin: EdgeInsets.only(top: deviceHeight * .08),
-                      child: Text('Mon Parcours', style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.w500, fontSize: 20
-                      ),),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: deviceHeight * .02),
-                height: deviceHeight * .2,
-                width: deviceWidth,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5)
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
+                      padding: EdgeInsets.only(left: deviceWidth * .05),
+                      height: deviceHeight * .17,
+                      width: deviceWidth,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(top: deviceHeight * .02),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(Icons.school, color: Colors.purple,),
+                                Container(
+                                  margin: EdgeInsets.only(left: deviceWidth * .02),
+                                  child: Text('Mon Parcours', style: TextStyle(
+                                      color: Colors.purple, fontWeight: FontWeight.w500, fontSize: 20
+                                  ),),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                     Container(
+                      padding: EdgeInsets.only(left: deviceWidth * .05),
                       margin: EdgeInsets.only(top: deviceHeight * .02),
-                      child: Text('Mon Parcours', style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.w500, fontSize: 20
-                      ),),
-                    )
+                      height: deviceHeight * .17,
+                      width: deviceWidth,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5)
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(top: deviceHeight * .02),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(Icons.school, color: Colors.purple,),
+                                Container(
+                                  margin: EdgeInsets.only(left: deviceWidth * .02),
+                                  child: Text('Status', style: TextStyle(
+                                      color: Colors.purple, fontWeight: FontWeight.w500, fontSize: 20
+                                  ),),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
---
+++ lib/screens/recherche.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RecherchePage extends StatefulWidget {
+  @override
+  _RecherchePageState createState() => _RecherchePageState();
+}
+
+class _RecherchePageState extends State<RecherchePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.pink,
+      ),
+    );
+  }
+}
