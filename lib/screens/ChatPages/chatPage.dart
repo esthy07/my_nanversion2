481796d99790c96db9a411e212aadd49be58f532
pentest225 <patrick.dagouaga@uvci.edu.ci>
2020-11-19 14:18:38 +0000

    patrick

--- lib/screens/ChatPages/chatPage.dart
+++ lib/screens/ChatPages/chatPage.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mynan/Constantes/customeTheme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -7,98 +9,36 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  Widget rdp(String rdse) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                      constraints: BoxConstraints(minWidth: 10, maxWidth: 300),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            bottomLeft: Radius.circular(5),
-                            topRight: Radius.circular(5)),
-                      ),
-                      child: Text(
-                        rdse,
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ))
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Text('12:24',
-                      style: TextStyle(fontSize: 10, color: Colors.grey)),
-                  Icon(Icons.check, size: 12),
-                ],
-              )
-            ],
-          )
-        ],
-      ),
-    );
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getMessageStream();
   }
 
-  Widget msg(String msg) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                      constraints: BoxConstraints(minWidth: 10, maxWidth: 300),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            bottomRight: Radius.circular(5),
-                            topRight: Radius.circular(5)),
-                      ),
-                      child: Text(
-                        msg,
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ))
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Text('12:24',
-                      style: TextStyle(fontSize: 10, color: Colors.grey)),
-                  Icon(Icons.check, size: 12),
-                ],
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  TextEditingController messageText = TextEditingController();
   static const menuItems = <String>[
     'One',
     'Two',
   ];
+
+  void getMessage() async {
+    final messages = await _firestore.collection("messages").get();
+    for (var i in messages.docs) {
+      print(i.data());
+    }
+  }
+
+  void getMessageStream() async {
+    await for (var snapshot in _firestore.collection("messages").snapshots()) {
+      for (var i in snapshot.docs) {
+        print(i.data());
+      }
+    }
+  }
+
   String _btn3SelectedVal;
   final List<PopupMenuItem<String>> _popUpMenuItems = menuItems
       .map(
@@ -138,7 +78,7 @@ class _ChatPageState extends State<ChatPage> {
               decoration: BoxDecoration(
                 color: Colors.grey[200],
                 image: DecorationImage(
-                 fit: BoxFit.cover,
+                  fit: BoxFit.cover,
                   image: AssetImage('assets/images/backgroundNaN.png'),
                 ),
               ),
@@ -158,17 +98,9 @@ class _ChatPageState extends State<ChatPage> {
                           )
                         ],
                       ),
-                      msg("Bonjour tu vas bien ? je suis un peu occupé on cause après bien ? je suis un peu occupé on cause après bien ? je suis un peu occupé on cause après"),
-                      rdp("Bonjous"),
-                      msg('oui et toi?'),
-                      msg("Bien et toi ?"),
-                      rdp("Bonjour tu vas bien ? je suis un peu occupé on cause après"),
-                      rdp("Bonjour tu vas bien ? je suis un peu occupé on cause après"),
-                      rdp("Bonjour tu vas bien ? je suis un peu occupé on cause après"),
-                      msg('oui et toi?'),
-                      msg("Bien et toi ?"),
-                      rdp("Bonjour"),
-                      rdp("?"),
+                      reightMessage(context,
+                          "Bonjour tu vas bien ? je suis un peu occupé on cause après bien ? je suis un peu occupé on cause après bien ? je suis un peu occupé on cause après"),
+                      leftMessage(context, "Bonjous"),
                     ],
                   )
                 ],
@@ -183,6 +115,7 @@ class _ChatPageState extends State<ChatPage> {
                 width: MediaQuery.of(context).size.width / 1.3,
                 alignment: Alignment.center,
                 child: TextField(
+                  controller: messageText,
                   decoration: InputDecoration(
                       hintText: 'Taper votre message',
                       border: InputBorder.none),
@@ -193,7 +126,12 @@ class _ChatPageState extends State<ChatPage> {
                   Icons.send,
                   color: Theme.of(context).primaryColor,
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  _firestore.collection('messages').add({
+                    "message": messageText.text,
+                    "sender": _auth.currentUser.email
+                  });
+                },
               ),
             ],
           ),
