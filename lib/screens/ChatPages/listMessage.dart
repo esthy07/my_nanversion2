42179cbc427b4d206ee93902347d2a1e988dcabc
pentest225 <patrick.dagouaga@uvci.edu.ci>
2020-12-02 17:09:28 +0000

    je crois que le beug est  bon

--- lib/screens/ChatPages/listMessage.dart
+++ lib/screens/ChatPages/listMessage.dart
@@ -94,10 +94,12 @@ class _ListMessageState extends State<ListMessage> {
                 final salons = snapshot.data.docs.reversed;
                 for (var salon in salons) {
                   var dateLastMessage = salon.get("lastMessage")["dateAdd"];
+                  print("Last date ");
+                  print(dateLastMessage);
                   String image = "";
                   String titre = "";
-                 final user1 = salon.get("users")[0] ;
-                  final user2 = salon.get("users")[1] ;
+                  final user1 = salon.get("users")[0];
+                  final user2 = salon.get("users")[1];
                   if (_auth.currentUser.email == user1["email"]) {
                     titre = user2["email"];
                     image = user2["image"];
@@ -105,10 +107,9 @@ class _ListMessageState extends State<ListMessage> {
                     titre = user1["email"];
                     image = user1["image"];
                   }
-                  // dateLastMessage =
-                  //     DateTime.parse(dateLastMessage.toDate().toString());
                   dateLastMessage =
-                      DateTime.now();
+                      DateTime.parse(dateLastMessage.toDate().toString());
+                  // dateLastMessage = DateTime.now();
                   listSalon.add(detailListMessage(
                       context: context,
                       heure: dateLastMessage,
